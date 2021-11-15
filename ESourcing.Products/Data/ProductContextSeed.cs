cd3bbc288dde292d14c3c83cba57c373386e315a
erool <erool_87@hotmail.com>
2021-11-15 15:02:06 +0300

    Add project files.

---
+++ ESourcing.Products/Data/ProductContextSeed.cs
@@ -0,0 +1,39 @@
+﻿using ESourcing.Products.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESourcing.Products.Data
+{
+    public class ProductContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if(!existProduct)
+            {
+                productCollection.InsertManyAsync(GetConfigureProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetConfigureProducts()
+        {
+            List<Product> productList = new List<Product>();
+            for (int i = 0; i < 10; i++)
+            {
+                Product pro = new Product();
+                pro.Name = i+" Name";
+                pro.Summary = i + " Summary";
+                pro.Description = i + " description";
+                pro.Price = new Random().Next(100,10000);
+                pro.ImageFile = "product_"+i;
+                pro.Category = i+"_Smart";
+
+                productList.Add(pro);
+            }
+            return productList;
+        }
+    }
+}
---
+++ ESourcing.Products/Startup.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ESourcing.Products.Data;
+using ESourcing.Products.Data.Interfaces;
+using ESourcing.Products.Repository;
+using ESourcing.Products.Repository.Interfaces;
+using ESourcing.Products.Settings;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ESourcing.Products
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<ProductDatabaseSettings>(Configuration.GetSection(nameof(ProductDatabaseSettings)));
+            services.AddSingleton<IProductDatabaseSettings>(sp => sp.GetRequiredService<IOptions<ProductDatabaseSettings>>().Value);
+
+            services.AddTransient<IProductContext, ProductContext>();
+            services.AddTransient<IProductRepository, ProductRepository>();
+            services.AddControllers();
+
+            services.AddSwaggerGen(p=> 
+            {
+                p.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title="ESourcing.Products",
+                    Version="v1"
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(p=>p.SwaggerEndpoint("/swagger/v1/swagger.json","ESourcing.Products v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
