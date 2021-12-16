eda01bc0b89ce503a3fe536875c07864a5cf79a0
erol <erolkirnavoglu@gmail.com>
2021-12-16 23:58:47 +0300

    Fosp-11 Order  and rabbitmq development

---
+++ ESourcing.Order/ESourcing.Order/Controllers/OrderController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESourcing.Order.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        
+    }
+}
